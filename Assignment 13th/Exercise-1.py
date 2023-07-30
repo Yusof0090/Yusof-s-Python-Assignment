ce2df309caaa18741bcd004e3c47b80a4d328f07
Yusof Shokohyan <138888595+Yusof0090@users.noreply.github.com>
2023-07-30 11:30:47 +0500

    new update
    I have added for loop in my assignment....

--- Assignment 13th/Exercise-1.py
+++ Assignment 13th/Exercise-1.py
@@ -1,8 +1,22 @@
 input_value=input("how much you want to print?")
+
+####### while loop #########
 def print_message(message,repeat_count=1):
     x=1
     while x <= repeat_count:
         print(message)
         x+=1
 
-print_message("hello world",int(input_value))
+print_message("hello world",input_value)
+
+####### for loop ##########
+
+def print_msg(msg,count=1):
+    for x in range(0,count):
+        if x <= count:
+            print(msg)
+        x+=1
+
+print_msg('hello',input_value)
+
+
